12ceed97a9a6897b7dfe9a1ef5c819cf25b6eded
unknown <himanshu bhanderi>
2019-12-26 09:45:23 -0800

    prectical 1 compelte

--- prectical1/Program.cs
+++ prectical1/Program.cs
@@ -6,7 +6,24 @@ namespace prectical1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            pattern();
         }
+        static void pattern()
+        {
+            int i, j;
+            for(i=0;i<5;i++)
+            {
+                 for(j=0;j<5;j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+
+
     }
 }
