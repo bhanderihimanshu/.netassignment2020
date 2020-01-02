b0e6163ad990a6402ac43e50f10fcc612d4a0bec
unknown <himanshu bhanderi>
2020-01-01 20:09:18 -0800

    prc 2 done

--- prectical2/Program.cs
+++ prectical2/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace prectical2
 {
@@ -7,12 +11,25 @@ namespace prectical2
         static void Main(string[] args)
         {
             string first_name, country;
-            Console.WriteLine("Enter your name:");
-            first_name = Console.ReadLine();
-            Console.WriteLine("where are you from?");
-            country= Console.ReadLine();
-            Console.WriteLine("Hello" + first_name + "from" + country);
-                
+            while (true)
+            {
+                Console.WriteLine("Enter your name:");
+                first_name = Console.ReadLine();
+                Console.WriteLine("where are you from?");
+                country = Console.ReadLine();
+
+                if (!(Regex.IsMatch(first_name, @"^[a-z A-Z]+$") && Regex.IsMatch(country, @"^[a-z A-Z]+$")))
+                {
+                    Console.WriteLine("NOT PROPER FORMAT,ONLY CHARCTOR ARE ALLOWED IS THE FORMAT\n");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Hello" + first_name + "from" + country);
+                    break;
+                }
+            }
+            Console.Read();
         }
     }
 }
