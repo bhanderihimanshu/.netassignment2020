0ce41925866036c0c5ff2893e26ea24fb1efcef8
unknown <himanshu bhanderi>
2020-01-01 19:28:04 -0800

    prectical 2 done

---
+++ prectical2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace prectical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first_name, country;
+            Console.WriteLine("Enter your name:");
+            first_name = Console.ReadLine();
+            Console.WriteLine("where are you from?");
+            country= Console.ReadLine();
+            Console.WriteLine("Hello" + first_name + "from" + country);
+                
+        }
+    }
+}
